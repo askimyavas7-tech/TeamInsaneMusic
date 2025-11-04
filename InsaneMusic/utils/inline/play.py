7209b019c24b9b27c2932d759244708f88e31c24
askimyavas7-tech <askimyavas7@gmail.com>
2025-11-04 19:19:05 +0300

    Update developer button URLs in play.py

--- InsaneMusic/utils/inline/play.py
+++ InsaneMusic/utils/inline/play.py
@@ -48,9 +48,9 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
             InlineKeyboardButton(text="✚", callback_data=f"add_playlist|{chat_id}"),
         ],
         [
-            InlineKeyboardButton(text="[🇮🇳] Dᴇᴠ", url=f"https://t.me/always_hungry365"),
+            InlineKeyboardButton(text="[✌🏻] Dᴇᴠ", url=f"https://t.me/trendyolmuzik_bot"),
             InlineKeyboardButton(
-                text="Cʜᴀᴛ Gʀᴏᴜᴘ", url=f"https://t.me/Dosto_ki_Mehfil786"
+                text="Cʜᴀᴛ Gʀᴏᴜᴘ", url=f"https://t.me/trendyolmuzik_bot"
             ),
         ],
         [
@@ -215,11 +215,11 @@ def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
         [
             InlineKeyboardButton(
                 text=_["P_B_1"],
-                callback_data=f"InsaneMusicPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
+                callback_data=f"TredyolPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
             ),
             InlineKeyboardButton(
                 text=_["P_B_2"],
-                callback_data=f"InsaneMusicPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
+                callback_data=f"TrendyolPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
             ),
         ],
         [
